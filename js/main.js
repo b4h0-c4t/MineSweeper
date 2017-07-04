e7cf91c552314c06d70df5a0fc066026acc4507b
HodakaHashiba <Hodaka@DokkaNoDareka.local>
2017-07-04 23:11:31 +0900

    add main function and template

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const generateBasicField = (num, len) => {
+  let remain = num;
+  return Array.from({length: len}, (obj, i) => {
+    if(remain / (len - i) > Math.random()) {
+      remain--;
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+};
+
+const generateField = (num, x, y) => {
+  return generateBasicField(num, x * y).map((obj, x) => {
+    return obj;
+  });
+};
+
+addEventListener('DOMContentLoaded', () => {
+  //canvas settings
+  const canvas = document.querySelector('#canvas');
+  const ctx = canvas.getContext('2d');
+
+  console.log(generateField(10, 10, 10));
+});
